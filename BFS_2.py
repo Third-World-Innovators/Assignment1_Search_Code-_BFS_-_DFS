fe5ba6a0ff62df484f6a65a368547cebaf2e8155
KaeloMedupe <72712572+Kaelo-twi@users.noreply.github.com>
2020-11-12 15:27:31 +0200

    Working code for BFS

--- BFS_2.py
+++ BFS_2.py
@@ -1,27 +1,40 @@
 graph = {
-  'A' : ['B','C'],
-  'B' : ['D', 'E'],
-  'C' : ['F'],
-  'D' : [],
-  'E' : ['F'],
-  'F' : []
+  'S': ['c', 'd', 'e'],
+  'b': ['i', 'j', 'q'],
+  'c': ['e', 'q'],
+  'd': ['b'],
+  'e': ['f', 'h'],
+  'f': [],
+  'G': [],
+  'h': [],
+  'i': [],
+  'j': [],
+  'p': ['G'],
+  'q': ['G', 'p']
 }
 
 visited = [] # List to keep track of visited nodes.
 queue = []     #Initialize a queue
-
+h=[]
 def bfs(visited, graph, node):
   visited.append(node)
   queue.append(node)
 
   while queue:
     s = queue.pop(0) 
-    print (s, end = " ") 
-
+    #print (s, end = " ")
+    h.append(s)
     for neighbour in graph[s]:
       if neighbour not in visited:
         visited.append(neighbour)
         queue.append(neighbour)
 
 # Driver Code
-bfs(visited, graph, 'A')
+bfs(visited, graph, 'S')
+k=[]
+for i in h:
+    if i=='G':
+        k.append(i)
+        break
+    k.append(i)
+print(k)
